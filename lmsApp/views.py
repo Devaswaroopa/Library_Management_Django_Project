612a8055721e6160440e679ebf9c7e545cb91d6d
EC2 Default User <ec2-user@ip-172-31-46-36.eu-west-3.compute.internal>
2023-04-26 22:23:09 +0000

    Working

--- lmsApp/views.py
+++ lmsApp/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib.auth.decorators import login_required
 
 
-
 def context_data(request):
     fullpath = request.get_full_path()
     abs_uri = request.build_absolute_uri()
@@ -149,22 +148,71 @@ def profile(request):
     return render(request,'profile.html', context)
 
 
-# new libraries used here
 @login_required
 def users(request):
-    return users_lib(request)
+    context = context_data(request)
+    context['page'] = 'users'
+    context['page_title'] = "User List"
+    context['users'] = User.objects.exclude(pk=request.user.pk).filter(is_superuser = False).all()
+    return render(request, 'users.html', context)
 
 @login_required
 def save_user(request):
-    return save_user_lib(request)
-    
+    resp = { 'status': 'failed', 'msg' : '' }
+    if request.method == 'POST':
+        post = request.POST
+        if not post['id'] == '':
+            user = User.objects.get(id = post['id'])
+            form = forms.UpdateUser(request.POST, instance=user)
+        else:
+            form = forms.SaveUser(request.POST) 
+
+        if form.is_valid():
+            form.save()
+            if post['id'] == '':
+                messages.success(request, "User has been saved successfully.")
+            else:
+                messages.success(request, "User has been updated successfully.")
+            resp['status'] = 'success'
+        else:
+            for field in form:
+                for error in field.errors:
+                    if not resp['msg'] == '':
+                        resp['msg'] += str('<br/>')
+                    resp['msg'] += str(f'[{field.name}] {error}')
+    else:
+         resp['msg'] = "There's no data sent on the request"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
 @login_required
 def manage_user(request, pk = None):
-    return manage_user_lib(request)
+    context = context_data(request)
+    context['page'] = 'manage_user'
+    context['page_title'] = 'Manage User'
+    if pk is None:
+        context['user'] = {}
+    else:
+        context['user'] = User.objects.get(id=pk)
+    
+    return render(request, 'manage_user.html', context)
 
 @login_required
 def delete_user(request, pk = None):
-    return delete_user_lib(request)
+    resp = { 'status' : 'failed', 'msg':''}
+    if pk is None:
+        resp['msg'] = 'User ID is invalid'
+    else:
+        try:
+            User.objects.filter(pk = pk).delete()
+            messages.success(request, "User has been deleted successfully.")
+            resp['status'] = 'success'
+        except:
+            resp['msg'] = "Deleting User Failed"
+
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+    
+    
 @login_required
 def category(request):
     context = context_data(request)
