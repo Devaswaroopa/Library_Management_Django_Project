9f495d8196f66a4ebfb59cce1311d71682117e84
EC2 Default User <ec2-user@ip-172-31-46-36.eu-west-3.compute.internal>
2023-04-26 22:04:07 +0000

    Working

--- library/user.py
+++ library/user.py
@@ -5,8 +5,9 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from lmsApp import models, forms
-
-
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 
 
